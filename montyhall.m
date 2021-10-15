ac97595eca3a688307bc8a22456c18bb92b02a9f
nolan4 <42417162+nolan4@users.noreply.github.com>
2021-10-14 19:02:50 -0700

    Update montyhall.m

--- montyhall.m
+++ montyhall.m
@@ -3,6 +3,7 @@
 num_games = 10000;
 
 counts = [0, 0];
+record_probs = zeros(num_games, 2);
 for i = 1:num_games
 
     % generate new location for grand prize
@@ -23,11 +24,20 @@ for i = 1:num_games
         counts(1) = counts(1) + 1;
     end
 
+    record_probs(i,:) = [counts(1)/i, counts(2)/i];
+
     if rem(i,1000) == 0
+        subplot(2,1,1)
         bar(counts/i)
         set(gca, 'xticklabel', {'keep'; 'switch'})
         ylim([0,1])
         title('Monty Hall Probabilities')
+        
+
+        subplot(2,1,2)
+        plot((1:i), record_probs(1:i,:), '-')
+        ylim([0,1])
+
         drawnow
     end
 
